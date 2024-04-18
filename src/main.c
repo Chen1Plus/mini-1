054e163ff82dc902ae59057d962c27beab3308e3
1plus <122733139+Chen1Plus@users.noreply.github.com>
2024-04-18 17:33:44 +0800

    code quality

--- src/main.c
+++ src/main.c
@@ -7,22 +7,11 @@
 #include "parser.h"
 
 int main() {
-    Node* root;
-    while ((root = statement())) {
-        if (root->tok == END) goto next_loop;
-
-        // printPrefix(root);
-        // printf("\n");
+    for (Node *root; (root = statement()); freeTree(root)) {
+        if (root->tok == END) continue;
         evaluateTree(root);
-
-        // printf("Result: r%d\nPrefix: ", evaluateTree(root));
         printf("\n");
-
-    next_loop:
-        freeTree(root);
     }
     getXYZ();
-    // for (int i = 0; i < 8; i++) ownReg(i);
-    // printf("x: r%d\ny: r%d\nz: r%d\n", getSym("x"), getSym("y"), getSym("z"));
     printf("EXIT 0\n");
 }
